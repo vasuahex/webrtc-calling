3f0b97bc87ff5fd29538a98a79d3a1bf75eee1ff
venu reddy <venugopalreddy9493@gmail.com>
2024-09-17 07:23:24 +0530

    changes for the routes

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -2,11 +2,15 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='p-4 h-screen'>
-      <div className='h-full flex-grow  p-5 border-black border-dotted border-2'>
+    <div className='m-5 h-screen border-black border-dotted border-2'>
+      <div className='h-full flex-grow space-y-3 p-5 '>
         <Link to="/adwancedraganddrop" className='px-3 py-2 border-2 rounded-sm bg-blue-300'>advance drag and drop</Link>
         <Link to="/drag" className='px-3 py-2 border-2 rounded-sm bg-yellow-100'>simple drag and drop</Link>
         <Link to="/draganddrop" className='px-3 py-2 border-2 rounded-sm bg-green-300'>basic drag and drop</Link>
+        <Link to="/toasts" className='px-3 py-2 border-2 rounded-sm bg-yellow-100'>custom toasts</Link>
+        <Link to="/pintrest" className='px-3 py-2 border-2 rounded-sm block min-w-[200px] w-fit bg-blue-300'>scroll images</Link>
+        <Link to="/videocall" className='px-3 py-2 border-2 rounded-sm block min-w-[200px] w-fit bg-green-300'>Video Call</Link>
+        <Link to="/waveform" className='px-3 py-2 border-2 rounded-sm block min-w-[200px] w-fit bg-yellow-100'>waveform</Link>
       </div>
 
     </div>
